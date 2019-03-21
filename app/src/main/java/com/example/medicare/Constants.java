8abb87201561f55d6af4b1bafaed003f754d4656
aniladepu007 <ced16i002@iiitdm.ac.in>
2019-03-21 17:48:27 +0530

    modified reisterPatient

--- app/src/main/java/com/example/medicare/Constants.java
+++ app/src/main/java/com/example/medicare/Constants.java
@@ -2,7 +2,7 @@ package com.example.medicare;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://192.168.1.113/MediCare/includes/";
+    private static final String ROOT_URL = "http://172.17.4.115/MediCare/includes/";
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
 
 }
