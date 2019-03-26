d89cc53fff15d9694ec00fbfae3922bf1dfd2308
aniladepu007 <ced16i002@iiitdm.ac.in>
2019-03-27 02:51:17 +0530

    changed constants

--- app/src/main/java/com/example/medicare/Constants.java
+++ app/src/main/java/com/example/medicare/Constants.java
@@ -2,8 +2,8 @@ package com.example.medicare;
 
 public class Constants {
 
-    private static final String ROOT_URL = "http://192.168.137.14/MediCare/includes/";
-    public static final String URL_LOGIN = ROOT_URL + "userLogin.php";
+    private static final String ROOT_URL = "http://192.168.43.243/MediCare/includes/";
+    public static final String URL_LOGIN = ROOT_URL + "patientLogin.php";
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
 
 
