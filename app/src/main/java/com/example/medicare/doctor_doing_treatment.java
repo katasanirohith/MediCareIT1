7463360ea6fe169ca17a0dbd597f546ef47dd1b4
katasanirohith <katasanirohith@gmail.com>
2019-04-04 14:07:20 +0530

    added all activities

---
+++ app/src/main/java/com/example/medicare/doctor_doing_treatment.java
@@ -0,0 +1,13 @@
+package com.example.medicare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class doctor_doing_treatment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_doing_treatment);
+    }
+}
