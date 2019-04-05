0881c426bd9a991bfe2e990c5229a57d08368848
aniladepu007 <ced16i002@iiitdm.ac.in>
2019-04-06 00:45:18 +0530

    debugged cardview

--- app/src/main/java/com/example/medicare/doctor_doing_treatment.java
+++ app/src/main/java/com/example/medicare/doctor_doing_treatment.java
@@ -2,8 +2,13 @@ package com.example.medicare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class doctor_doing_treatment extends AppCompatActivity {
+
+    EditText textTreatmentDate, textSlot, textSymptoms, textdiagnosis, textPrescription, textRemarks;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
